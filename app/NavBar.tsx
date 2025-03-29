292e62ba3e0e8e2367c51dfb10b980b61fdca030
sktbrd <sktbrd.eth@gmail.com>
2025-03-29 12:08:37 -0300

    remove "Contato" link from navigation bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,7 +15,7 @@ import { Link } from '@chakra-ui/next-js';
 import { CiMenuBurger } from "react-icons/ci";
 import { FaHome, FaRegWindowClose } from "react-icons/fa";
 
-const Links = ["Sobre", "Projetos", "Agradecimentos", "Contato", "Jadson Brian"];
+const Links = ["Sobre", "Projetos", "Agradecimentos", "Jadson Brian"];
 
 const NavLink = ({ children }: { children: string }) => (
     <Link
